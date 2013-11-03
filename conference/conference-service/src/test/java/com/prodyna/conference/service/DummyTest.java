70f548cdd98a4182cea3176f6fab718c8792e759
Frank Herling <fherling@prodyna.com>
2013-11-03 18:49:44 +0100

    Sonntag

---
+++ conference/conference-service/src/test/java/com/prodyna/conference/service/DummyTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.prodyna.conference.service;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+/**
+ * @author fherling
+ *
+ */
+public class DummyTest {
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+	}
+}
